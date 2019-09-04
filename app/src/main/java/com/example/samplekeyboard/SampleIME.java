df96df296f80ae4cb609a8c1f88c38ad5e1f77bf
mayancn <mayanchanchani@gmail.com>
2019-09-05 00:27:33 +0530

    added physical keyboard settings, made softkeyboad layout better

--- app/src/main/java/com/example/samplekeyboard/SampleIME.java
+++ app/src/main/java/com/example/samplekeyboard/SampleIME.java
@@ -13,10 +13,13 @@ public class SampleIME extends InputMethodService implements KeyboardView.OnKeyb
     KeyboardView keyboardView;
     Keyboard keyboard;
     boolean caps;
+    boolean lang;
 
     @Override
     public View onCreateInputView() {
         super.onCreateInputView();
+        caps = false;
+        lang = false;
         keyboardView = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
         keyboard = new Keyboard(this, R.xml.marathi);
         keyboardView.setKeyboard(keyboard);
@@ -44,6 +47,7 @@ public class SampleIME extends InputMethodService implements KeyboardView.OnKeyb
                     keyboardView.setOnKeyboardActionListener(this);
                 }else{
                     caps = !caps;
+//                    keyboard = new Keyboard(this, R.xml.marathi);
                     keyboard = new Keyboard(this, R.xml.marathi);
                     keyboardView.setKeyboard(keyboard);
                     keyboardView.setOnKeyboardActionListener(this);
@@ -51,6 +55,19 @@ public class SampleIME extends InputMethodService implements KeyboardView.OnKeyb
                 break;
             case Keyboard.KEYCODE_DONE:
                 inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            case Keyboard.KEYCODE_ALT:
+                if(lang){
+                    lang = !lang;
+                    keyboard = new Keyboard(this, R.xml.qwerty);
+                    keyboardView.setKeyboard(keyboard);
+                    keyboardView.setOnKeyboardActionListener(this);
+                }else{
+                    lang = !lang;
+                    keyboard = new Keyboard(this, R.xml.marathi);
+                    keyboardView.setKeyboard(keyboard);
+                    keyboardView.setOnKeyboardActionListener(this);
+                }
                 break;
             default:
                 char code = (char) primaryCode;
